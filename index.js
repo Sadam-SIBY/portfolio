59d1a522a381dcbe2402964b58c4f75d75b79811
SadamOclock <sadam.siby@oclock.school>
2024-04-19 23:05:08 +0200

    boucle

--- index.js
+++ index.js
@@ -55,9 +55,10 @@ var imageUrls   = [
 
 // var imageNumber = imageUrls.length
 
+
 for (var i = 0; i < imageUrls.length; i++){
-  var imageNumber = imageUrls[i].length
-  console.log(imageNumber);
+  var imageNumber = imageUrls[i]
+  console.log (imageNumber);
 }
 
 
@@ -69,126 +70,85 @@ function imagePop(id){
     var imageName = document.getElementById(id + '-image').style.backgroundImage
 
 
-
-
-    // var imageIndex = 0
+    var imageIndex = 0
+    imageIndex = imageNumber.indexOf(imageName)
   
-    for (var i = 0; i < imageUrls.length; i++){
-
-      i.array.forEach(element => {
-        imageIndex = element.indexOf(imageName)
- 
-      });
-
-      // for (var imageNumber = 0 ;  imageNumber < imageUrls[i].length; imageNumber++){
-      //   imageIndex = imageNumber.indexOf(imageName)
-        
-      //   console.log(imageNumber);
-   
-      // }
-    }
-    
-
-    // imageIndex = imageUrls.indexOf(imageName)
     
     document.getElementsByClassName('imageContain')[0].style.animation = 'he 800ms forwards'
 
-    document.getElementsByClassName('imageContain')[0].style.backgroundImage = imageUrls[i][imageIndex]
+    document.getElementsByClassName('imageContain')[0].style.backgroundImage = imageUrls[imageIndex]
 }
 
 
 function imageChange(id){
 
+
     var imageName = document.getElementById(id).style.backgroundImage
-
-    var imageIndex = 0
-    imageIndex = imageUrls.indexOf(imageName)
-    var imageNumber = imageUrls[imageNumber]
     
+
+    // var imageIndex = 0
+
     for (var i = 0; i < imageUrls.length; i++){
-
-      i.array.forEach(element => {
-        imageIndex = element.indexOf(imageName)
-
-        if(imageIndex >= i.length - 1)
-        imageIndex = 0
-        else
-        imageIndex++
-      });
-
+      var imageNumber = imageUrls[i]
+      imageIndex = imageUrls[i].indexOf(imageName)
+      if(imageIndex >= imageNumber - 1)
+      imageIndex = 0
+      else
+      imageIndex++
+      console.log (imageNumber);
+   
     }
+
+
+    document.getElementById(id).style.backgroundImage = imageUrls[i][imageIndex]
   
-
-    // if(imageIndex >= imageNumber - 1)
-    // imageIndex = 0
-    // else
-    // imageIndex++
-
-
-    document.getElementById(id).style.backgroundImage = imageUrls[imageIndex]
-
 }
-
 function imageMoveLeft(){
 
     var imageName = document.getElementById('imageContainId').style.backgroundImage
 
-    var imageIndex = 0
-    imageIndex = imageUrls.indexOf(imageName)
-    if(imageIndex <= 0 )
-    imageIndex = imageNumber - 1
-    else
-    imageIndex--
+    // var imageIndex = 0
+    for (const img = 0 ; img < imageUrls.length ; img++){
+      imageIndex = imageUrls[img].indexOf(imageName)
 
-    console.log (imageIndex);
-
-    // for (var i = 0; i < imageUrls.length; i++){
-    //   var imageIndex = 0
-    //     for (var imageNumber = 0 ;  imageNumber < imageUrls[i].length; imageNumber++){
-    //       imageIndex = imageUrls[imageNumber ].indexOf(imageName)
-    //         var imageNumber = imageUrls[imageNumber]
-    //         if(imageIndex <= 0)
-    //         imageIndex = imageNumber - 1
-    //         else
-    //         imageIndex--
-    //       }
-    // }
+ 
+   
+      if(imageIndex <= 0 )
+      imageIndex = imageNumber - 1
+      else
+      imageIndex-
+      console.log (imageIndex);
+   
 
     document.getElementById('imageContainId').style.backgroundImage = imageUrls[imageIndex]
 
-
+  }
 }
 
 function imageMoveRight(){
 
     var imageName = document.getElementById('imageContainId').style.backgroundImage
 
-    // var imageIndex = 0
-    // imageIndex = imageUrls.indexOf(imageName)
-    // if(imageIndex >= imageNumber - 1)
-    // imageIndex = 0
-    // else
-    // imageIndex++
-    var imageIndex = 0
-
-
     for (var i = 0; i < imageUrls.length; i++){
-        for (var imageNumber = 0 ;  imageNumber < imageUrls[i].length; imageNumber++){
-          imageIndex = imageUrls[imageNumber].indexOf(imageName)
-            var imageNumber = imageUrls[imageNumber]
-            if(imageIndex >= imageNumber - 1)
-            imageIndex = 0
-            else
-            imageIndex++
-          }
-    }
+      var imageNumber = imageUrls[i]
+      imageIndex = imageUrls[i].indexOf(imageName)
+    
+  
 
+   
+    if(imageIndex >= imageNumber - 1)
+    imageIndex = 0
+    else
+    imageIndex++
 
+    
+console.log (imageNumber )
+  
+ 
     document.getElementById('imageContainId').style.backgroundImage = imageUrls[i][imageIndex]
-
+  }
 
 }
-
 
 function imagePopNone(){
 
@@ -197,16 +157,3 @@ function imagePopNone(){
 
 }
 
-// --------------------------
-
-// var imgUrls = [
-//                 [
-//                   'url("assets/images/oboulot.png")','url("assets/images/connexion.png")'
-//                 ],
-//                 [
-//                   'url("assets/images/bord.png")','url("assets/images/classe.png")','url("assets/images/exo.png")','url("assets/images/addQuestion.png")','url("assets/images/btp.png")','url("assets/images/eleve.png")'
-//                 ],
-//                 [
-//                   'url("assets/images/AlloFlix.png")', 'url("assets/images/AlloFlixAdd.png")'
-//                 ]
-// ]
